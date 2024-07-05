7ab46277e02feb88d8d87872f640b867c31e4c11
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2024-07-04 23:30:03 -0300

    feat: refactor list user

--- app/Repositories/EloquentRepository/RoleRepository.php
+++ app/Repositories/EloquentRepository/RoleRepository.php
@@ -14,110 +14,109 @@ use Throwable;
 
 class RoleRepository extends AbstractRepository implements RoleRepositoryInterface
 {
-    public function __construct(private Role $role)
-    {
-        $this->role = $role;
+  public function __construct(private Role $role)
+  {
+    $this->role = $role;
+  }
+
+  public function list(CustomPaginateParamsDTO $paramsDTO): LengthAwarePaginator
+  {
+    $query = $this->role->query();
+    $query->when($paramsDTO->search, function () use ($paramsDTO, $query) {
+      $query->where('name', 'ilike', "%{$paramsDTO->search}%");
+    })->when($paramsDTO->sortBy, function () use ($paramsDTO, $query) {
+      $order = $paramsDTO->descending ? 'desc' : 'asc';
+      $query->orderBy($paramsDTO->sortBy, $order);
+    });
+
+    return $query->paginate($paramsDTO->rowsPerPage ?? 10);
+  }
+
+  public function create(CreateRoleDTO $roleDTO): Model|Role
+  {
+    DB::beginTransaction();
+
+    try {
+      $role = $this->role->create([
+        'name' => $roleDTO->name,
+        'guard_name' => 'web',
+        'slug' => str()->slug($roleDTO->name),
+        'description' => $roleDTO->description,
+      ]);
+      $role->syncPermissions($roleDTO->permissions);
+
+      DB::commit();
+
+      return $role;
+    } catch (Throwable $throw) {
+      DB::rollBack();
+
+      throw $throw;
     }
+  }
 
-    public function list(CustomPaginateParamsDTO $paramsDTO): LengthAwarePaginator
-    {
-        $query = $this->role->query();
+  public function getById(int $id): Model|Role
+  {
+    return $this->role->with(['permissions' => function ($query) {
+      return $query->select(['id', 'description']);
+    }])->findOrFail($id);
+  }
 
-        $query->when($paramsDTO->search, function () use ($paramsDTO, $query) {
-            $query->where('name', 'ilike', "%{$paramsDTO->search}%");
-        })->when($paramsDTO->sortBy, function () use ($paramsDTO, $query) {
-            $order = $paramsDTO->descending ? 'desc' : 'asc';
-            $query->orderBy($paramsDTO->sortBy, $order);
-        });
+  public function update(UpdateRoleDTO $roleDTO): Model|Role
+  {
+    DB::beginTransaction();
 
-        return $query->paginate($paramsDTO->rowsPerPage ?? 10);
+    try {
+
+      $role = $this->role->findOrFail($roleDTO->id);
+
+      $role->update([
+        'name' => $roleDTO->name,
+        'guard_name' => 'web',
+        'slug' => str()->slug($roleDTO->name),
+        'description' => $roleDTO->description,
+      ]);
+
+      if ($roleDTO->permissions !== null) {
+        $role->syncPermissions($roleDTO->permissions);
+      }
+
+      DB::commit();
+
+      return $role;
+    } catch (Throwable $throw) {
+      DB::rollBack();
+
+      throw $throw;
     }
+  }
 
-    public function create(CreateRoleDTO $roleDTO): Model|Role
-    {
-        DB::beginTransaction();
+  public function delete(int $id): bool
+  {
+    DB::beginTransaction();
 
-        try {
-            $role = $this->role->create([
-                'name' => $roleDTO->name,
-                'guard_name' => 'web',
-                'slug' => str()->slug($roleDTO->name),
-                'description' => $roleDTO->description,
-            ]);
-            $role->syncPermissions($roleDTO->permissions);
+    try {
+      $role = $this->role->findOrFail($id);
 
-            DB::commit();
+      DB::commit();
 
-            return $role;
-        } catch (Throwable $throw) {
-            DB::rollBack();
+      return $role->delete();
+    } catch (Throwable $throw) {
+      DB::rollBack();
 
-            throw $throw;
-        }
+      throw $throw;
     }
+  }
 
-    public function getById(int $id): Model|Role
-    {
-        return $this->role->with(['permissions' => function ($query) {
-            return $query->select(['id', 'description']);
-        }])->findOrFail($id);
-    }
+  public function getBySlug(string $slug): Model|Role
+  {
+    return $this->role->with(['permissions' => function ($query) {
+      return $query->select(['id', 'description']);
+    }])->where('slug', $slug)->firstOrFail();
+  }
 
-    public function update(UpdateRoleDTO $roleDTO): Model|Role
-    {
-        DB::beginTransaction();
-
-        try {
-
-            $role = $this->role->findOrFail($roleDTO->id);
-
-            $role->update([
-                'name' => $roleDTO->name,
-                'guard_name' => 'web',
-                'slug' => str()->slug($roleDTO->name),
-                'description' => $roleDTO->description,
-            ]);
-
-            if ($roleDTO->permissions !== null) {
-                $role->syncPermissions($roleDTO->permissions);
-            }
-
-            DB::commit();
-
-            return $role;
-        } catch (Throwable $throw) {
-            DB::rollBack();
-
-            throw $throw;
-        }
-    }
-
-    public function delete(int $id): bool
-    {
-        DB::beginTransaction();
-
-        try {
-            $role = $this->role->findOrFail($id);
-
-            DB::commit();
-
-            return $role->delete();
-        } catch (Throwable $throw) {
-            DB::rollBack();
-
-            throw $throw;
-        }
-    }
-
-    public function getBySlug(string $slug): Model|Role
-    {
-        return $this->role->with(['permissions' => function ($query) {
-            return $query->select(['id', 'description']);
-        }])->where('slug', $slug)->firstOrFail();
-    }
-
-    public function listAll(): Collection
-    {
-        return $this->role->all(['id', 'name']);
-    }
+  public function listAll(): Collection
+  {
+    return $this->role->all(['id', 'name']);
+  }
 }
