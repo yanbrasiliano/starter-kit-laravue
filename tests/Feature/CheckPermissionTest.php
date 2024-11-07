a92de7b7b639643f6f9d9480bef85dd47463ae00
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2024-11-07 11:05:27 -0300

    fix: ajustando testes

--- tests/Feature/CheckPermissionTest.php
+++ tests/Feature/CheckPermissionTest.php
@@ -4,14 +4,20 @@ use App\Http\Middleware\CheckPermission;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 describe('CheckPermission Middleware', function () {
-    it('should return an access denied error', function () {
-        $userAuth = User::factory()->create();
-        Auth::login($userAuth);
-        $request = Request::create('/test', 'GET');
-        $middleware = new CheckPermission();
-        $middleware->handle($request, function ($req) {
-        });
-    })->throws(Exception::class, 'Você não tem permissão para realizar esta ação.');
+  it('should return an access denied error', function () {
+    $userAuth = User::factory()->create();
+
+    Auth::login($userAuth);
+
+    Route::shouldReceive('currentRouteName')->andReturn('view_permission');
+
+    $request = Request::create('/test', 'GET');
+    $middleware = new CheckPermission();
+
+    $middleware->handle($request, function ($req) {
+    });
+  })->throws(Exception::class, 'Você não tem permissão para realizar esta ação.');
 })->group('middleware');
