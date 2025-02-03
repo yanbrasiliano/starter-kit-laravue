232797a4e54b2ca5c4514d8437296153a777283a
nikoz84 <nikoz.1984@gmail.com>
2025-02-03 15:20:18 -0300

    chore: adicionando configurações iniciais

---
+++ config/starterkit.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Configurações iniciais
+    |--------------------------------------------------------------------------
+    |
+    | Aqui voce pode configurar as opções para: usuários, perfis, permissões entre outras configurações.
+    |
+    */
+
+    'admin_email' => env('APP_ADMIN_EMAIL'),
+    'admin_password' => env('APP_ADMIN_PASSWORD'),
+    'admin_role' => env('APP_ADMIN_ROLE'),
+
+];
