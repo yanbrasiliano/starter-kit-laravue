1dc8fec6d476ea27685344112e23d1218b5d1b85
nikoz84 <nikoz.1984@gmail.com>
2025-02-07 13:15:33 -0300

    docs: adicionando injeção dos documentos necessários para ver o DER

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,56 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\{File, Route};
 
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '.*');
+})->where('any', '^(?!der|assets|schema.json).*');
+
+Route::get('/der', function () {
+    $key = request()->get('key');
+
+    if (!request()->has('key')) {
+        return abort(404);
+    }
+
+    if (request()->get('key') !== config('starterkit.der.key')) {
+        return abort(401);
+    }
+
+    return response()->file(
+        public_path('dist/index.html'),
+        ['Content-Type' => 'text/html; charset=utf-8']
+    );
+});
+
+Route::get('/schema.json', function () {
+    $filePath = public_path('dist/schema.json');
+
+    if (!File::exists($filePath)) {
+        abort(500, "Schema.json file not found.");
+    }
+
+    return response()->file($filePath, [
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+    ]);
+});
+
+Route::get('/assets/{path}', function ($path) {
+    $filePath = public_path("dist/assets/{$path}");
+
+    if (!File::exists($filePath)) {
+        abort(404);
+    }
+
+    $mimeType = match(pathinfo($filePath, PATHINFO_EXTENSION)) {
+        'js' => 'application/javascript; charset=utf-8',
+        'css' => 'text/css',
+        'ico' => 'image/x-icon',
+        'png' => 'image/png',
+        default => File::mimeType($filePath)
+    };
+
+    return response()->file($filePath, ['Content-Type' => $mimeType]);
+})->where('path', '.*');
