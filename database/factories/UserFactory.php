9b106e91fd11f82703a655952e531b75c99171ac
nikoz84 <nikoz.1984@gmail.com>
2025-02-22 03:58:11 -0300

    test: aplicando mudanças em testes para melhorar o coverage

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -16,7 +15,7 @@ class UserFactory extends Factory
             'cpf' => preg_replace('/\D/', '', fake('pt_BR')->cpf()),
             'active' => true,
             'email_verified_at' => now(),
-            'password' => Hash::make('admin'),
+            'password' => 'admin',
             'remember_token' => Str::random(10),
         ];
     }
@@ -25,6 +24,13 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'active' => 'false',
         ]);
     }
 }
