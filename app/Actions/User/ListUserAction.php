a6c4b93a79ca1adc8c981282d815da14231a0878
nikoz84 <nikoz.1984@gmail.com>
2025-02-25 01:02:34 -0300

    refactor: mudando a action pattern

---
+++ app/Actions/User/ListUserAction.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Actions\User;
+
+use App\Models\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Fluent;
+
+final readonly class ListUserAction
+{
+    public function execute(Fluent $params): LengthAwarePaginator|Collection
+    {
+        $query = User::query();
+
+        $query->select([
+            'users.*',
+            'roles.name as role',
+            'role_user.role_id as role_id',
+        ])
+            ->leftJoin('role_user', 'users.id', '=', 'role_user.user_id')
+            ->leftJoin('roles', 'role_user.role_id', '=', 'roles.id');
+
+        $query->when($params->search, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('users.id', 'ilike', "%$search%")
+                    ->orWhere('users.name', 'ilike', "%$search%")
+                    ->orWhere('users.email', 'ilike', "%$search%")
+                    ->orWhere('roles.name', 'ilike', "%$search%");
+            });
+        });
+
+        $query->when($params->order, function ($query, $order) use ($params) {
+            $column = match ($params->column) {
+                'name' => 'users.name',
+                'email' => 'users.email',
+                'role' => 'roles.name',
+                'setSituation' => 'users.active',
+                default => 'users.id',
+            };
+            $query->orderBy($column, $order);
+        });
+
+        return $params->paginated ? $query->paginate($params->limit ?? 10) : $query->get();
+    }
+}
