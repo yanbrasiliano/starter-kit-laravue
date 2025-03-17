8d21516fa2ff1341cbe7340662a8f151a8b75893
Everton Novaes <evertonnfs2@gmail.com>
2025-03-17 12:09:41 -0300

    fix: ajusta o menu para funcionar corretamente no mobile

--- resources/js/layouts/AdminLayout.vue
+++ resources/js/layouts/AdminLayout.vue
@@ -1,24 +1,27 @@
 <script setup>
 import useAuthenticate from '@/composables/Authenticate/useAuthenticate';
 import { useQuasar } from 'quasar';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import AdminHeaderLayout from './AdminHeaderLayout.vue';
 import AdminSidebar from './AdminSidebar.vue';
+import logoImage from '@assets/logo-125px40px.png';
 
 const drawer = ref(true);
 const miniState = ref(false);
 const route = useRoute();
 const { myProfile } = useAuthenticate();
+const $q = useQuasar();
+
+const isMobile = computed(() => $q.screen.width <= 1024);
 
 onMounted(async () => {
   await myProfile();
 });
-const $q = useQuasar();
 </script>
 
 <template>
-  <q-layout view="lHh Lpr lff" container style="height: 300px" class="layout__admin">
+  <q-layout v-if="!isMobile" view="lHh Lpr lff" container class="layout__admin">
     <AdminHeaderLayout>
       <div v-if="route?.meta?.module && route?.meta?.icon" class="desktop-only">
         <span class="text-h4">
@@ -40,6 +43,42 @@ const $q = useQuasar();
       @mouseleave="miniState = drawer ? miniState : true"
       :class="$q.dark.isActive ? 'bg-primary' : 'bg-primary'">
       <AdminSidebar v-model:miniState="miniState"></AdminSidebar>
+    </q-drawer>
+
+    <q-page-container class="bg__grey--style">
+      <q-page padding class="q-mt-md">
+        <router-view></router-view>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+
+  <q-layout v-else
+    view="hHh Lpr lff"
+    container
+    style="height: 300px"
+    class="layout__admin header-mobile">
+    <q-header elevated class="bg-white">
+      <q-toolbar class="q-pl-sm q-pr-md">
+        <img :src="logoImage" width="115px" />
+        <q-toolbar-title class="text-dark flex flex-center q-pr-xl q-pt-xs">{{ route?.meta?.module }}</q-toolbar-title>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" class="text-dark"></q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :mini="miniState"
+      @mouseenter="miniState = false"
+      @mouseleave="miniState = true"
+      mini-to-overlay
+      :mini-width="120"
+      :width="238"
+      :breakpoint="500"
+      bordered
+      :class="$q.dark.isActive ? 'bg-primary' : 'bg-primary'">
+
+      <AdminSidebar v-model:miniState="miniState" :isMobile="isMobile"></AdminSidebar>
     </q-drawer>
 
     <q-page-container class="bg__grey--style">
--- resources/js/layouts/AdminSidebar.vue
+++ resources/js/layouts/AdminSidebar.vue
@@ -1,13 +1,25 @@
 <script setup>
-import logoSmaller from '@assets/icons/logoSmaller-50px40px.png';
+import logoSmaller from '@assets/logoSmaller-50px40px.png';
 import logoImage from '@assets/logo-125px40px.png';
 import { hasPermission } from '@utils/hasPermission';
 import { isActiveLink } from '@utils/isActiveLink';
 import { ROLE_PERMISSION, USER_PERMISSION } from '@utils/permissions';
+import useAuthStore from '@/store/useAuthStore';
+import useAuthenticate from '@/composables/Authenticate/useAuthenticate';
+import { useRouter } from 'vue-router';
 
 const props = defineProps({
   miniState: Boolean,
+  isMobile: Boolean,
 });
+
+const authStore = useAuthStore();
+const { logout } = useAuthenticate();
+const router = useRouter();
+
+const goToEditProfile = () => {
+  router.push({ name: 'editUsers', params: { id: authStore.getUser?.id } });
+};
 
 const emit = defineEmits(['update:miniState']);
 </script>
@@ -22,7 +34,7 @@ const emit = defineEmits(['update:miniState']);
         :to="{ name: 'adminHome' }"
         :class="isActiveLink('adminHome')">
         <q-item-section avatar class="text-white">
-          <q-icon name="space_dashboard" />
+          <q-icon name="sym_o_space_dashboard" />
         </q-item-section>
         <q-item-section class="text-white"> Dashboard </q-item-section>
       </q-item>
@@ -37,7 +49,7 @@ const emit = defineEmits(['update:miniState']);
         :to="{ name: 'listUsers' }"
         :class="isActiveLink('listUsers')">
         <q-item-section avatar class="text-white">
-          <q-icon name="people_alt" />
+          <q-icon name="sym_o_people_alt" />
         </q-item-section>
         <q-item-section class="text-white"> Usuários </q-item-section>
       </q-item>
@@ -50,22 +62,51 @@ const emit = defineEmits(['update:miniState']);
         :to="{ name: 'listRoles' }"
         :class="isActiveLink('listRoles')">
         <q-item-section avatar class="text-white">
-          <q-icon name="contacts" />
+          <q-icon name="sym_o_contacts" />
         </q-item-section>
         <q-item-section class="text-white"> Perfis </q-item-section>
       </q-item>
     </q-list>
+
+    <div v-if="isMobile">
+      <q-separator style="background-color: #ffffff2b"></q-separator>
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar class="text-white">
+          <q-icon name="help" />
+        </q-item-section>
+        <q-item-section class="text-white"> Ajuda </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple @click="goToEditProfile">
+        <q-item-section avatar class="text-white">
+          <q-icon name="account_circle" />
+        </q-item-section>
+        <q-item-section class="text-white"> Meu Perfil </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple @click="logout">
+        <q-item-section avatar class="text-white">
+          <q-icon name="exit_to_app" />
+        </q-item-section>
+        <q-item-section class="text-white"> Sair </q-item-section>
+      </q-item>
+    </div>
   </q-scroll-area>
-  <div
-    v-if="!props.miniState"
-    class="logo__sidebar--active"
-    style="top: 65px; left: 55px">
-    <img :src="logoImage" width="124px" />
+
+  <div v-if="!isMobile">
+    <div
+      v-if="!props.miniState"
+      class="logo__sidebar--active"
+      style="top: 65px; left: 55px">
+      <img :src="logoImage" width="124px" />
+    </div>
+    <div v-else class="logo__sidebar--inactive" style="top: 65px; left: 20px">
+      <img :src="logoSmaller" width="50px" />
+    </div>
   </div>
-  <div v-else class="logo__sidebar--inactive" style="top: 65px; left: 20px">
-    <img :src="logoSmaller" width="50px" />
-  </div>
-  <div class="absolute" style="top: 80px; right: -11px">
+
+  <div v-if="!isMobile" class="absolute" style="top: 80px; right: -11px">
     <q-btn
       round
       dense
@@ -74,5 +115,14 @@ const emit = defineEmits(['update:miniState']);
       color="secondary"
       :icon="props.miniState ? 'chevron_right' : 'chevron_left'"
       @click="emit('update:miniState', !props.miniState)"></q-btn>
+  </div>
+
+  <div v-if="isMobile" class="absolute-top text-white avatar-mobile" style="height: 150px">
+    <div class="absolute-bottom bg-transparent">
+      <div size="56px" class="q-mb-sm">
+        <q-icon name="account_circle" size="56px" />
+      </div>
+      <div class="text-weight-bold">{{ authStore.getUser?.name }}</div>
+    </div>
   </div>
 </template>
