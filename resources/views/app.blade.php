63ce4c3d8f1aac5b75a9c4f87f1a3912f69c14ac
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-04-07 08:20:19 -0300

    fix: update favicon links for improved compatibility and remove deprecated entries

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,9 @@
     @if (app()->environment('development', 'staging'))
         <meta name="robots" content="noindex">
     @endif
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <title>Starter Kit</title>
     <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" href="data:,">
 
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
