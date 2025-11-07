2491f5ae9197f2c2f218ade5b1d207fc00671656
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-11-07 18:38:58 -0300

    fix(migration): add missing index definitions and correct DB statement for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
     public function up(): void {
@@ -17,7 +18,15 @@ return new class () extends Migration {
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->index('name');
+            $table->index('cpf');
+            $table->index('active');
+            $table->index('email_verified_at');
+            $table->index('created_at');
         });
+
+        DB::statement("CREATE INDEX users_name_tsv_idx ON users USING gin(to_tsvector('portuguese', name));");
     }
 
     public function down(): void {
