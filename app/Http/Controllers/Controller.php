339fb52a6ec14b7ba59ddcbd9a62e4c75cd85dd3
nikoz84 <nikoz.1984@gmail.com>
2025-11-09 18:48:59 -0300

    chore: adjust in base classes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,7 @@ use Illuminate\Routing\Controller as BaseController;
 
 // @codeCoverageIgnoreStart
 
-final class Controller extends BaseController {
+abstract class Controller extends BaseController {
     use AuthorizesRequests;
     use ValidatesRequests;
 }
--- app/Http/Resources/BaseResource.php
+++ app/Http/Resources/BaseResource.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Str;
  *
  * @property mixed $preserveKeys
  */
-final class BaseResource extends JsonResource {
+abstract class BaseResource extends JsonResource {
     /**
      * Adds pagination information to the response.
      *
